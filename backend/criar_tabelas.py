b2271d57fcd1b2f994c8767b3922af5768e00eae
olgapedrosa <olga.sousa@aluno.uece.br>
2025-07-06 14:51:22 -0300

    concertei a coreografia

---
+++ backend/criar_tabelas.py
@@ -0,0 +1,18 @@
+from db.connection import conectar
+import os
+
+def criar_tabelas():
+    conn = conectar()
+    cursor = conn.cursor()
+
+    caminho_schema = os.path.join(os.path.dirname(__file__), "db", "schema.sql")
+
+    with open(caminho_schema, "r", encoding="utf-8") as f:
+        cursor.executescript(f.read())
+
+    conn.commit()
+    conn.close()
+    print("Tabelas criadas com sucesso.")
+
+if __name__ == "__main__":
+    criar_tabelas()
